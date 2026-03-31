db2213aa28faf8ba620cf54f353ae54f3fbe12c7
Mauro <emauriciorg@gmail.com>
2026-03-31 12:46:17 +0000

    fix(compact) : added conflicted code

--- ms912x_compat.h
+++ ms912x_compat.h
@@ -3,14 +3,49 @@
 #ifndef MS912X_COMPAT_H
 #define MS912X_COMPAT_H
 
+#include <linux/string.h>
+#include <linux/types.h>
 #include <linux/version.h>
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 18, 0)
+#if __has_include(<linux/dma-buf-map.h>)
 #include <linux/dma-buf-map.h>
+#elif __has_include(<linux/dma-buf.h>)
+#include <linux/dma-buf.h>
+#endif
+
+#ifdef DMA_BUF_MAP_INIT_OFFSET
 #define iosys_map dma_buf_map
 #define IOSYS_MAP_INIT_OFFSET DMA_BUF_MAP_INIT_OFFSET
 #define iosys_map_memcpy_from dma_buf_map_memcpy_from
 #define iosys_map_incr dma_buf_map_incr
+#else
+struct iosys_map {
+	void *vaddr;
+};
+
+static inline struct iosys_map
+IOSYS_MAP_INIT_OFFSET(const struct iosys_map *src, size_t offset)
+{
+	struct iosys_map map = {
+		.vaddr = (u8 *)src->vaddr + offset,
+	};
+
+	return map;
+}
+
+static inline void iosys_map_memcpy_from(void *dst,
+					 const struct iosys_map *src,
+					 size_t offset, size_t len)
+{
+	memcpy(dst, (u8 *)src->vaddr + offset, len);
+}
+
+static inline void iosys_map_incr(struct iosys_map *map, size_t offset)
+{
+	map->vaddr = (u8 *)map->vaddr + offset;
+}
+#endif
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 11, 0)
